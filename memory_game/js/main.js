11610240ba885c4367c34fc47d5ea38db737f4c2
Mindy Soto <mindysoto@Mindy-Sotos-MBP.fios-router.home>
2019-11-14 10:35:22 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,14 @@
 console.log("Up and running!");
-
-
+//creates the game board by adding elements to the doc
+function createBoard(){
+	for (let i = 0; i < cards.length; i++){
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);	
+	}
+}
 
 const cards = [
 {
@@ -22,30 +30,44 @@ const cards = [
 	rank: "king",
 	suit: "diamonds",
 	cardImage: "images/king-of-diamonds.png"
-}];
+}
+];
 
 let cardsInPlay = [];
+//resets the game board using an event listener
+function resetBoard(){
+	for(let i = 0; i < cards.length; i ++){
+	var board = document.querySelector('img');
+	board.parentNode.removeChild(board);
+	}
+	createBoard(); 
+}
+document.getElementsByTagName('button')[0].addEventListener('click', resetBoard);
 
+//Checks that cards match and sends user alert
 function checkForMatch(){
-if (cardsInPlay[0] === cardsInPlay[1]){
+	if(cardsInPlay[0].rank === cardsInPlay[1].rank){
 	alert("You found a match.");
-}else {
+	} else{
 	alert("Sorry try again.");
-}
+	}
 }
 
-function flipCard(cardId){
-	console.log("User flipped " + cards[cardId].rank);
+//Flips cards and assigns card image
+function flipCard(){
+	var cardId = this.getAttribute('data-id');
 	cardsInPlay.push(cards[cardId]).rank;
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
-	if (cardsInPlay.length === 2){
+	this.setAttribute('src', cards[cardId].cardImage);
+	if(cardsInPlay.length === 2){
 		checkForMatch();
-	}
+		}
 }
-	
-flipCard(0);
-flipCard(2);
+
+createBoard();
+
+
 
 
 
